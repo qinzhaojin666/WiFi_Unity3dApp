12c4ba3d33279214f862d97cfa7340fd736ea4f7
BasePC1 <mail@razum.by>
2018-11-30 15:27:20 +0300

    User Interface

---
+++ Unity3d Wi-Fi App/Assets/Scripts/ButtonColorController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonColorController : MonoBehaviour {
+
+    Image button;
+
+    public bool activity = true;
+
+    void Start()
+    {
+
+       button = GetComponent<Image>();
+
+    }
+
+    void Update()
+    {
+
+
+        if (activity)
+        {
+            ActiveButton();
+        }
+        else
+        {
+            DisableButton();
+        }
+
+
+    }
+
+    void ActiveButton()
+    {
+
+        button.color = Color.white;
+
+    }
+    void DisableButton()
+    {
+
+        button.color = Color.red;
+
+    }
+    public void ChooseActivity()
+    {
+
+        if (activity)
+        {
+            activity = false;
+        }
+        else
+        {
+            activity = true;
+        }
+
+
+    }
+}
---
+++ Unity3d Wi-Fi App/Assets/Scripts/MenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+    public ButtonColorController b1;
+    public ButtonColorController b2;
+    public ButtonColorController b3;
+    public ButtonColorController b4;
+    public ButtonColorController b5;
+    public ButtonColorController b6;
+    public ButtonColorController b7;
+
+
+    // Update is called once per frame
+    public void StartAlgorythm ()
+    {
+        b1.activity = false;
+        b2.activity = false;
+        b3.activity = false;
+        b4.activity = false;
+        b5.activity = false;
+        b6.activity = false;
+        b7.activity = false;
+    }
+
+    public void StopAlgorythm()
+    {
+        b1.activity = true;
+        b2.activity = true;
+        b3.activity = true;
+        b4.activity = true;
+        b5.activity = true;
+        b6.activity = true;
+        b7.activity = true;
+    }
+}
