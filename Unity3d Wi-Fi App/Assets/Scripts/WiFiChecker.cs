00245115386232e4a29fe418cd98fce524b513ca
Andrew Kharkow <andrey-kh2100@mail.ru>
2018-12-01 22:07:06 +0300

    User Interface

---
+++ Unity3d Wi-Fi App/Assets/Scripts/WiFiChecker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WiFiChecker : MonoBehaviour
+{
+
+    Animator animator;
+
+    // Use this for initialization
+    void Start()
+
+    {
+        animator = GetComponent<Animator>();
+
+    }
+    public void WiFiCheckerFunction()
+    {
+
+        if (Application.internetReachability != NetworkReachability.NotReachable)
+        {
+
+            animator.SetBool("WiFi_Find", true);
+            Debug.Log("Wi-Fi is available");
+
+        }
+        else
+        {
+            animator.SetBool("WiFi_NotFind", true);
+            Debug.Log("Wi-Fi is not available");
+
+        }
+
+    }
+    public void ReturnToMainScreen()
+    {
+
+        animator.SetBool("WiFi_NotFind", false);
+
+    }
+}
+
---
+++ Unity3d Wi-Fi App/Assets/Scripts/WiFiIsFind.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WiFiIsFind : MonoBehaviour {
+
+    Animator animator;
+	
+	// Update is called once per frame
+	void Start () {
+
+        animator = GetComponent<Animator>();
+		
+	}
+
+    public void WriteIPAdress()
+    {
+
+        animator.SetBool("IP", true);
+
+    }
+}
